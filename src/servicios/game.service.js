d86c8668fa5ec500b5c0490e214df5186e2dd282
Ruben Castillo <Tartas_casr@hotmail.com>
2023-05-01 19:57:43 -0600

    cambios

---
+++ src/servicios/game.service.js
@@ -0,0 +1,24 @@
+import firebase from "../firebase/firebase";
+
+const db = firebase.collection("/videojuegos");
+
+class TutorialDataService {
+  getAll() {
+    return db;
+  }
+
+  create(juego) {
+    return db.add(juego);
+  }
+
+  update(id, value) {
+    return db.doc(id).update(value);
+  }
+
+  delete(id) {
+    return db.doc(id).delete();
+  }
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default new TutorialDataService();
